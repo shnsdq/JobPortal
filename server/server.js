0bc69b07f05d112deb7112781673705aadad7cdc
Shaheen <shn.sdq29@gmail.com>
2026-07-18 15:50:16 +0530

    update API endpoints to use production URLs

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:"https://jobportal-8aq7.onrender.com",
     credentials:true
 }));
 
