28af4ab2d714f3e7addf9b014a420309e9960221
Shaheen <shn.sdq29@gmail.com>
2026-09-09 22:47:03 +0530

    fix: enhance layout and styling in Browse and Job components

--- client/src/components/Job.jsx
+++ client/src/components/Job.jsx
@@ -15,8 +15,9 @@ const Job = ({ job }) => {
     const timeDifference = currentTime - createdAt
     return Math.floor(timeDifference / (1000 * 24 * 60 * 60));
   }
+  
   return (
-    <div className='p-5 rounded-md shadow-xl bg-white border-gray-100'>
+    <div className='rounded-md shadow-xl bg-white border-gray-100'>
       <div className='flex items-center justify-between'>
         <p className='text-sm text-gray-500'>{daysAgoFunction(job?.createdAt) === 0 ? "Today" : `${daysAgoFunction(job?.createdAt)} days ago`}</p>
         <Button variant="outline" className='rounded-full' size="icon"><Bookmark /></Button>
@@ -36,7 +37,9 @@ const Job = ({ job }) => {
 
       <div>
         <h1 className='font-bold text-lg my-2'>{job?.title}</h1>
+          <div className='overflow-hidden'>
         <p className='text-sm text-gray-600'>{job?.description}</p>
+        </div>
       </div>
       <div className='flex items-center gap-2 mt-4'>
         <Badge className='text-blue-700 font-bold' variant="ghost">{job?.position}</Badge>
@@ -44,7 +47,7 @@ const Job = ({ job }) => {
         <Badge className='text-[#7209b7] font-bold' variant="ghost">{job?.salary}</Badge>
       </div>
       <div className='flex items-center gap-4 mt-4'>
-        <Button onClick={() => navigate(`/description/${job?._id}`)} variant="outline">Details</Button>
+        <Button onClick={() => navigate(`/description/${job?._id}`)} variant="outline" className="cursor-pointer">Details</Button>
         <Button className="bg-[#7209b7]">Save For Later</Button>
       </div>
     </div>
