861f688e375088003ff20844c186b3ede0741bc2
Shaheen <shn.sdq29@gmail.com>
2026-09-12 22:56:03 +0530

    full height for better layout

--- client/src/components/LatestJobCards.jsx
+++ client/src/components/LatestJobCards.jsx
@@ -7,7 +7,7 @@ const LatestJobCards = ({ job }) => {
   const navigate = useNavigate();
 
   return (
-    <div onClick={() => navigate(`/description/${job._id}`)} className='p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer'>
+    <div onClick={() => navigate(`/description/${job._id}`)} className='p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer h-full'>
       <div>
         <h1 className='font-medium text-lg'>{job?.company?.name}</h1>
         <p className='text-sm text-gray-500'>{job?.company?.location}</p>
